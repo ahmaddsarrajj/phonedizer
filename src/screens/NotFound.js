f358c160ba69137a75caab178563adf408850a0e
ahmad sarraj <83416111+ahmaddsarrajj@users.noreply.github.com>
2022-04-25 15:56:57 -0700

    new change

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -8,7 +8,6 @@ const NotFound = () => {
       <div className="container my-5">
         <div className="row justify-content-center align-items-center">
           <h4 className="text-center mb-2 mb-sm-5">Page Not Found</h4>
-          
           <div className="col-md-3 col-sm-6">
             <Link to="/" className="text-white text-decoration-none">
             <button className="  col-12 btn btn-primary mt-5">
