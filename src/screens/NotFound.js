f66f66ab0969a15f55f99b59f4754c338fd9419d
ahmad sarraj <83416111+ahmaddsarrajj@users.noreply.github.com>
2022-04-25 15:55:32 -0700

    Update NotFound.js

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -5,7 +5,6 @@ import Header from "./../components/Header";
 const NotFound = () => {
   return (
     <>
-      <Header />
       <div className="container my-5">
         <div className="row justify-content-center align-items-center">
           <h4 className="text-center mb-2 mb-sm-5">Page Not Found</h4>
